85e7f2e1afd01182478c6ac18872d0f3ba4064e9
Deshana <desai.deshna@gmail.com>
2018-04-25 17:33:11 -0400

    EnsembleColor

--- augmentations/ensemble.py
+++ augmentations/ensemble.py
@@ -29,6 +29,12 @@ class Templates:
                           "Is a {} placed on the {} side of the {}?".format(obj1,pos,obj2)]
         return random.choice(base_templates)    
 
+    def color_templates(obj1, pos, obj2):
+        """Templates for questions of the Position Yes/No type.
+        """        
+        base_templates = ["What is the color of the {} on the {} of {}?".format(obj1,pos,obj2)]
+        return random.choice(base_templates)    
+
 class Ensemble:
     def __init__():
         self.templates = Templates()
@@ -52,21 +58,29 @@ class Ensemble:
         return obj1_string, obj2_string
                 
     def gen_obj_question(obj1,obj2,pos,adj1):
-        q = self.templates.obj_recog_templates(adjective_obj(adj1,adj2,obj1,obj2))
+        obj1, obj2 = adjective_obj(adj1,adj2,obj1,obj2)        
+        q = self.templates.obj_recog_templates(obj1, pos)
         return (q,obj2)
     
-    def gen_yes_or_no(obj1,obj2,pos,adj1,ans, adj2 = None):       
-        q = self.templates.yes_or_no_templates(adjective_obj(adj1,adj2,obj1,obj2))        
+    def gen_yes_or_no(obj1,obj2,pos,adj1,ans, adj2 = None): 
+        obj1, obj2 = adjective_obj(adj1,adj2,obj1,obj2)
+        q = self.templates.yes_or_no_templates()        
         return (q,ans)
         
-    def gen_pos_question(obj1,obj2,pos,adj1 = None, adj2 = None):                  
-        q = self.templates.pos_templates(adjective_obj(adj1,adj2,obj1,obj2))
+    def gen_pos_question(obj1,obj2,pos,adj1 = None, adj2 = None):
+        obj1, obj2 = adjective_obj(adj1,adj2,obj1,obj2)
+        q = self.templates.pos_templates(obj1, obj2)
         return (q,pos)
 
     def gen_count_question(obj1, pos, obj2, count, adj1 = None, adj2 = None):
-        q = self.templates.count_templates(adjective_obj(adj1,adj2,obj1,obj2))
+        obj1, obj2 = adjective_obj(adj1,adj2,obj1,obj2)
+        q = self.templates.count_templates(obj1, pos, obj2)
         return (q, count)       
     
+    def gen_color_question(obj1, pos, obj2, color, adj1 = None, adj2 = None):
+        obj1, obj2 = adjective_obj(adj1,adj2,obj1,obj2)        
+        q = self.templates.color_templates(obj1, pos, obj2)
+        return (q, color)        
     
             
            
